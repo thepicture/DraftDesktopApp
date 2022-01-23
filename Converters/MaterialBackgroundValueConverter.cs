3314b1a01e96d129188c8e45779ddcc7dbeca295
thepicture <85333076+thepicture@users.noreply.github.com>
2022-01-23 07:38:10 +0000

    Add highlighting of materials depending on their fields

---
+++ Converters/MaterialBackgroundValueConverter.cs
@@ -0,0 +1,37 @@
+﻿using DraftDesktopApp.Models.Entities;
+using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace DraftDesktopApp.Converters
+{
+    public class MaterialBackgroundValueConverter : IValueConverter
+    {
+        public object Convert(object value,
+                              Type targetType,
+                              object parameter,
+                              CultureInfo culture)
+        {
+            Material material = value as Material;
+            BrushConverter brushConverter = new BrushConverter();
+            if (material.CountInStock < material.MinCount)
+            {
+                return brushConverter.ConvertFrom("#f19292");
+            }
+            else if (material.CountInStock >= material.MinCount * 3)
+            {
+                return brushConverter.ConvertFrom("#ffba01");
+            }
+            return Brushes.White;
+        }
+
+        public object ConvertBack(object value,
+                                  Type targetType,
+                                  object parameter,
+                                  CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
